5ac2d7b8c49e61d63fe8b558cb7282cd7c3c5058
Andrii Chychkan <mywork1728@gmail.com>
2023-12-06 16:55:48 +0200

    added main navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Home from './pages/Home/Home';
+import Favorites from './pages/Favorites/Favorites';
+import Catalog from './pages/Catalog/Catalog';
+import NotFound from './pages/NotFound/NotFound';
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/catalog">Catalog</NavLink>
+        <NavLink to="/favorites">Favorites</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
