6dba5abeee0488db0226741c79329ad349fb7479
Andrii Chychkan <mywork1728@gmail>
2023-12-06 23:24:13 +0200

    started

---
+++ src/redux/operations.js
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://650c4d9447af3fd22f676f23.mockapi.io';
+
+export const fetchCars = createAsyncThunk(
+  'cars/fetchCars',
+  async ({ rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(`/adverts`);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
